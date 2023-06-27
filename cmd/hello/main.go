ea1092778e2c1444f8d6e2a3e051763ddefc668b
Kay Craig <kay.craig@hashicorp.com>
2023-06-27 12:34:32 -0400

    contrive a dependency
    4.0.0 is an intentionally out of date dependency

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -1,7 +1,16 @@
 package hello
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/go-faker/faker/v4"
+)
 
 func main() {
-	fmt.Println("hello world!")
+	fmt.Printf("hello %s!\n", getFake())
+}
+
+// contrive a dependency
+func getFake() string {
+	return faker.FirstName()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/kpcraig/gh-source
 
 go 1.20
+
+require github.com/go-faker/faker/v4 v4.0.0
+
+require golang.org/x/text v0.3.7 // indirect
